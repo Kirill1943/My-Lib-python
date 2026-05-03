e32713d5dbb13b6c50e0c87058b571dfcb07dbe2
kirill1943 <kirillsok2015@gmail.com>
2026-05-03 19:26:06 +0400

    0.3.0-alpha2: вторая альфа, небольшие изменения файла шифров

--- kirilltools/cipher.py
+++ kirilltools/cipher.py
@@ -1,7 +1,7 @@
 from kirilltools.errors.math import TypesError
 import kirilltools.errors.cipherError as err
 
-def Caesar_cipher(text: str, next: int = 1024):
+def Caesar_cipher(text: str, next: int = 1024) -> str:
     """
     распостраненный шифр цезаря который сдвигает 
     каждую букву на несколько позиций вперед в unicode
@@ -16,7 +16,7 @@ def Caesar_cipher(text: str, next: int = 1024):
         return_text = return_text + str(i)
     return return_text
 
-def custom_cipher(text: str, key: list):
+def custom_cipher(text: str, key: list) -> dict:
     """
     это функция позволяющая зашифровать своим ключом
     сообщение. работает это по принципу списков в списке, пример:
@@ -25,22 +25,39 @@ def custom_cipher(text: str, key: list):
     вернет bbb
     """
     try:
-        # 1. Проверяем, что ключ - это список
         if not isinstance(key, list):
             raise err.KeyFormatError("Ключ должен быть списком! (list)")
 
         for item in key:
-            # 2. Проверяем, что каждый элемент - тоже список из 2 элементов
             if not isinstance(item, list) or len(item) != 2:
                 raise err.KeyFormatError("Каждый элемент ключа должен быть списком [old, new]!")
             
             old, new = item
-            # 3. Сама замена
             text = text.replace(str(old), str(new))
     except Exception as e:
-        # Если случилось что-то совсем странное
         raise err.KeyFormatError(f"Битый / неверный ключ! Ошибка: {e}") from None
 
     return text
 
 
+def keys() -> dict:
+    """
+    просто функция которая возвращает разные ключи
+    """
+    dict_keys = {
+        "Ru.Ru > En.Us": [
+            ["А", "A"], ["В", "B"], ["Е", "E"], ["К", "K"], 
+            ["М", "M"], ["Н", "H"], ["О", "O"], ["Р", "P"], 
+            ["С", "C"], ["Т", "T"], ["Х", "X"], ["а", "a"], 
+            ["е", "e"], ["о", "o"], ["р", "p"], ["с", "c"], 
+            ["у", "y"], ["х", "x"]
+        ],
+        "En.Us > Ru.Ru": [
+            ["A", "А"], ["B", "В"], ["E", "Е"], ["K", "К"], 
+            ["M", "М"], ["H", "Н"], ["O", "О"], ["P", "Р"], 
+            ["C", "С"], ["T", "Т"], ["X", "Х"], ["a", "а"], 
+            ["e", "е"], ["o", "о"], ["p", "р"], ["c", "с"], 
+            ["y", "у"], ["x", "х"]
+        ]
+    }
+    return dict_keys
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='Kiriltools',
-    version='0.3.0-alpha1',
+    version='0.3.0-alpha2',
     packages=find_packages(),
     install_requires=[
         'httpx', 'h11', 'httpcore', 'anyio', # httpx и зависимости
         'psutil' # psutil
     ],
-    description="были добавлены маленькие изменения math модуля и модуль шифрования"
+    description="были добавлены 2 вида ключа в файл chiper.py и прокачан справочный модуль"
 )
